b267b2dc8d9b11c244b6a525d328fa722a7b3f28
Akash9933 <Akash9933@hotmail.ca>
2022-11-22 13:50:53 -0800

    Added Javascript Elements

---
+++ script.js
@@ -0,0 +1,36 @@
+function displayA() {
+    var x= document.getElementById("hideA");
+    if (x.style.display === "none") {
+        x.style.display ="block";
+    } else {
+        x.style.display ="none";
+    }
+}
+
+
+function displayP() {
+    var x= document.getElementById("hideP");
+    if (x.style.display === "none") {
+        x.style.display ="block";
+    } else {
+        x.style.display ="none";
+    }
+}
+
+function displayF() {
+    var x= document.getElementById("hideF");
+    if (x.style.display === "none") {
+        x.style.display ="block";
+    } else {
+        x.style.display ="none";
+    }
+}
+
+function displayL() {
+    var x= document.getElementById("hideL");
+    if (x.style.display === "none") {
+        x.style.display ="block";
+    } else {
+        x.style.display ="none";
+    }
+}
